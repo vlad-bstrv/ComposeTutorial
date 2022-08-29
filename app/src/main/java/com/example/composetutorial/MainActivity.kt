c3c5878cf35ffadff87d22e2d57f7a0d1f126963
vladislavbystrov <keocera@icloud.com>
2022-08-29 15:52:24 +0300

    Lesson 2: Layouts

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,31 +3,56 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(text = "ololo")
+            MessageCard(Message("Vlad", "Android"))
         }
     }
 }
 
 @Composable
-fun MessageCard(text: String) {
-    Text(text = "hello world!! $text")
+fun MessageCard(text: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = text.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = text.body)
+        }
+    }
+    
+
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(text = "ololo")
+    MessageCard(Message("Vlad", "Android"))
 }
---
+++ app/src/main/java/com/example/composetutorial/Message.kt
@@ -0,0 +1,6 @@
+package com.example.composetutorial
+
+data class Message(
+    val author: String,
+    val body: String
+)
